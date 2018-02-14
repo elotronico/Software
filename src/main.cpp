0756c6580b8ac99cef73155bc2cdfa08963176b2
elotronico <eloy_tronic@hotmail.es>
2018-02-14 10:09:48 +0100

    Update main.cpp
    Update main file.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
-#include "mbed.h"
+ #include "mbed.h"
 #include "OpenChair.h"
 
-AnalogIn potX(PA_0);
-DigitalIn pulsador(PA_10);    //D2
-//DigitalIn pulsadorDOWN(PB_3);   //bajar velocidad
+DigitalIn hombremuerto(D2);   //Pulsador Hombre muerto
+DigitalIn cambiarmarcha(D3);  // Pulsador incrementar marchas
+DigitalOut ledmarcha1(D4);    //led indicador marcha 1 metida
+DigitalOut ledmarcha2(D5);    //led indicador marcha 2 metida
+//DigitalOut ledmarcha3(D6);    //led indicador marcha 3 metida
+
 
 Serial pc(SERIAL_TX, SERIAL_RX);
 OpenChair chair(PC_12,PD_2,PC_10,PC_11,60,150); //50,150
@@ -13,41 +16,59 @@ void acelera(int);
 void frena(int);
 void manten(void);
 
+int marcha1 = 9,
+    marcha2 = 3,
+    marcha3 = 3;
 int marcha = 0;
 double Ma = 0, Mb = 0;
-double potenciometroX = 0;
+
 
 //####################################################################
 //                   MAIN
 //####################################################################
 int main() {
+ledmarcha1.write(0);
+ledmarcha2.write(0);
+//ledmarcha3.write(0);
+
+
     pc.baud(115200);
 
     while(1){
-          potenciometroX =potX.read();
-          pc.printf("%f \t %d\n", potenciometroX, marcha);
+          pc.printf("%f \t %d\n", hombremuerto, marcha);
 
-          //si el poteneciometro es mayor de 0.8, habilitamos el interruptor  de acelerar.
-          if( potenciometroX > 0.8 ){
-          pc.printf("pulsador habilitado \t");
+          if( hombremuerto == 1 ){
+          pc.printf("hombre muerto habilitado \t");
 
-            if (pulsador == 1)  {
+            if (cambiarmarcha == 1)  {
                    if (marcha == 0){
                         marcha = 1;
+                        ledmarcha1.write(1);
+                        ledmarcha2.write(0);
+                        //ledmarcha3.write(0);
                         pc.printf("MARCHA 1\n");
-                        acelera(6);  }
+                        acelera(marcha1);  }
                     else if (marcha == 1){
                         marcha = 2;
+                        ledmarcha1.write(0);
+                        ledmarcha2.write(1);
+                        //ledmarcha3.write(0);
                         pc.printf("MARCHA 2\n");
-                        acelera(2);  }
+                        acelera(marcha2);  }
                     else if (marcha == 2){
                         marcha = 3;
+                        ledmarcha1.write(1);
+                        ledmarcha2.write(1);
+                        //ledmarcha3.write(1);
                         pc.printf("MARCHA 3\n");
-                        acelera(2); }
+                        acelera(marcha3); }
                     else if (marcha == 3){
                         marcha = 1;
+                        ledmarcha1.write(1);
+                        ledmarcha2.write(0);
+                        //ledmarcha3.write(0);
                         pc.printf("MARCHA 1\n");
-                        frena(4);  }
+                        frena(marcha2+marcha3);         }
 
                     else{
                         pc.printf("- - - - ERROR - - - - \n");
@@ -63,19 +84,28 @@ int main() {
 
         //valor pot muy bajo. paramos silla
         else{
-            pc.printf("pot muy bajo. Paramos.\n");
+            pc.printf("hombre muerto no pulsado. Paramos.\n");
 
             if(marcha == 1){
                 marcha = 0;
-                frena(6);   }
+                ledmarcha1.write(0);
+                ledmarcha2.write(0);
+                //ledmarcha3.write(0);
+                frena(marcha1);   }
 
             else if(marcha == 2){
                 marcha = 0;
-                frena(8);  }
+                ledmarcha1.write(0);
+                ledmarcha2.write(0);
+                //ledmarcha3.write(0);
+                frena(marcha1+marcha2);  }
 
-            else if (marcha == 3){
+            else if(marcha == 3){
                 marcha = 0;
-                frena(4);  }
+                ledmarcha1.write(0);
+                ledmarcha2.write(0);
+                //ledmarcha3.write(0);
+                frena(marcha1+marcha2+marcha3);  }
 
             else{
                 chair.writeMotor1(0,0);
@@ -93,19 +123,19 @@ void frena(int z){
 
   for(int i=0; i<z; i++){
       pc.printf("%d f\n", i);
-      chair.writeMotor1( 65505, (int)1);
-      chair.writeMotor2( 65565, (int)1);
-      wait_ms(250);
+      chair.writeMotor1( 65480, (int)1); //65460
+      chair.writeMotor2( 65590, (int)1); //65610
+      wait_ms(90);//250
       }
 }
 
 void acelera(int z){
 
-  for(int i=0; i<z; i++){
+  for(int i=0; i<(2*z); i++){
       pc.printf("%d a\n", i);
-      chair.writeMotor1( 65565, (int)1);
-      chair.writeMotor2( 65505, (int)1);
-      wait_ms(250);
+      chair.writeMotor1( 65565, (int)1); //65565
+      chair.writeMotor2( 65505, (int)1); //65505
+      wait_ms(100);//250
       }
 }
 
